ad62cf4510d877e939648708684b49c246e47eec
Facu-SH <fundacosh@gmail.com>
2024-11-19 13:32:33 -0300

    hotfix

---
+++ ejercicio.js
@@ -0,0 +1,16 @@
+//Variables
+//let nombre = Facundo;
+//let edad = 28;
+//let profesion = Contador;
+
+
+let nombre = prompt("Ingresa tu nombre:");
+let edad = prompt("Ingresa tu edad:");
+let profesion = prompt("Ingresa tu profesión:");
+let dias = edad * 365;
+
+let mensajeUsuario = nombre + "Tenés " + edad + " y tu profesión es " + profesion + "\n";
+let mensajeUsuario2 = "Viviste " + dias + " días desde que naciste :D";
+
+alert(mensajeUsuario);
+alert(mensajeUsuario2);
